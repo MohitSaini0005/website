a8a6b2b9ffc82758983343b9669296a159ba1443
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-15 04:03:13 +0000

    conversion

---
+++ .devcontainer/web/conversion.js
@@ -0,0 +1,22 @@
+//let score = "33abc"
+
+ //console.log(typeof score);
+ //console.log(typeof(score));
+ //let valueInNumber = Number(score)
+ //console.log(valueInNumber);
+ //"33"=> 33
+//"33abc" => NaN
+//ture => 1 ;flase =>0
+
+let isLoggedIn =1
+
+//let boooleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(isLoggedIn)
+
+//1 => true 0=> flase
+//"" flase 
+//"vlase" => ture
+
+//let someNumber = 33
+//let stringNumber = String(someNumber)
+//consloe.log(stringNumber);
