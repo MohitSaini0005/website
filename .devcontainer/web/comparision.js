313e47b1d65c72d1f86ea1ec9a4fc106b57031c7
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-16 02:43:35 +0000

    change

---
+++ .devcontainer/web/comparision.js
@@ -0,0 +1,9 @@
+// console.log(2==1);
+// console.log(2<=5);
+// console.log(null > 0);
+// console.log(null == 0);// auoid this condition
+// console.log(null >= 0);
+// in java js null >= 0 treat is  like null > 0 true than ture is ture
+
+//==== check data type 
+console.log("2" === 2);// thses condition check data type 
---
+++ .devcontainer/web/operation.js
@@ -0,0 +1,15 @@
+// let str1 = 1;
+// let str2 =2;
+// let str =str1+str2;
+// console.log(str);
+
+// console.log(2/3/+5/5%6);//it is not working 
+// console.log("2" +2+ 43)
+// //traat as a string if strat with the string 
+// console.log(2 + 2 +"4")
+// console.log(ture);
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
