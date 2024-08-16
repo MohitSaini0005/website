f11df8c3169678dc8d96ba6e443259ad14cd235c
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-16 03:11:06 +0000

    interviewdata

---
+++ .devcontainer/web/datatypeforINTERVIEW.js
@@ -0,0 +1,15 @@
+// permitive data type 
+//primative   call by value 
+// string , number , Boolean , null , undifined , Symbol, Bigint 
+  // referance data type other word NON- perimtive 
+  // Array , FUncion,OBject 
+
+  const score =100
+  const scroei =false 
+  let myobj={
+    name:"mohit ",
+  }
+   const myFunction = function(){
+    console.log("hello world");
+   }
+   
