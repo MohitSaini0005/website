431b3253b1154b5d4f14c0aa6071e4f4c8232896
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-17 04:20:07 +0000

    deepString

---
+++ .devcontainer/web/deepStringdata.js
@@ -0,0 +1,24 @@
+// const name = "mohit ";
+// const repoCount =50
+
+// console.log(name + repoCount);
+// console.log(`hello my name is ${name} and my repo cunnt is ${repoCount}`);
+// const gameName = new Sting('mohit ')
+// console.log(gameName[2]);
+//onsole.log("hello");
+const gameBoll = new String('ramjilal')
+//console.log(gameBoll.length);
+console.log(gameBoll.toUpperCase());
+console.log(gameBoll.indexOf('t'));
+
+const newString  = gameBoll.substring(0,3)
+console.log(newString);
+
+const moString = gameBoll.slice(-8,9);
+console.log(MessagePort);
+
+// const mString = gameBoll.trim();
+// console.log(mString);
+// const url  = "https.//mohti.com/mohti20mohit"
+// console.log(url.replace(`%20`,`_`))
+console.log(gameBoll.split('-'))
