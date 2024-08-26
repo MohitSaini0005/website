045476d60958122883285784fd9438eaccbd953b
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-26 09:01:26 +0000

    hello

---
+++ .devcontainer/web/Reducemethos.js
@@ -0,0 +1,59 @@
+// // reduce method 
+
+//  const myNUm = [ 1,2,3,4,5,6,7,8,9]
+// //  const mytotal =  myNUm.reduce(function(acc , current){
+// //     console.log(`acc: ${acc} and cureent:${current}`);
+    
+// //     return acc + current
+// //  },6)
+
+// //const myTotal = myNUm.reduce ((acc,curr) => acc+curr,0 )
+
+
+//  console.log(myTotal);
+
+
+// const shpooingApp [
+//     {
+//         itemName: "Java script ",
+//         price : 25333
+// 5
+//     },
+//     {
+//         itemName: "Java  ",
+//         price : 23353
+
+//     },
+
+//     {
+//         itemName: " advance Java  ",
+//         price : 23353
+
+//     },
+// ]
+
+// console.log("hello every onre");
+// const shoppingCart = [
+//     {
+//         itemName : "java course ",
+//         price : 1234
+//     },
+//     {
+//         itemName : "dsa course ",
+//         price : 1234
+//     },
+//     {
+//         itemName : "html course ",
+//         price : 1234
+//     },
+//     {
+//         itemName : "j Advance ava course ",
+//         price : 1234
+//     },
+// ]
+
+//  const price =shoppingCart.reduce(( acc , item) =>  acc + item.price , 0)
+
+//  console.log(price);
+ 
+
--- .devcontainer/web/looping1.js
+++ .devcontainer/web/looping1.js
@@ -1,1 +1,14 @@
-bgfgn 
+const myNum = [1,2,3,4,5,6,7,8,8,10] 
+ //const newNUM = myNum.map( (num) => num+10)
+
+
+
+
+ const newNum = myNum.map((num) => num*10)
+ .map((num)=> num+10)
+ .filter( (num) => num>40)
+ console.log(newNum);
+ 
+
+ 
+ 
