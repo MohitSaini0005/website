62ee9c5691cbf6af155ea0949fd757c1afda0168
MOHIT KUMAR SAINI <mohitchatgpt0@gmail.com>
2024-08-26 02:43:00 +0000

    loops

---
+++ .devcontainer/web/for.js
@@ -0,0 +1,21 @@
+const myObject = {
+    js: 'javascript ',
+    cpp:  ' c++',
+    html: 'hyper text markup languange',
+    css: 'cascadig style language' 
+} 
+
+ for (const key in myObject) {
+//console.log(`${key} shortcut of for => ${myObject[key]}`);
+
+   
+ } 
+  
+ const programming = ["js","python","helo"]
+    for (const key in programming) {
+        console.log(programming[key]);
+        
+       
+        
+    
+    }
---
+++ .devcontainer/web/loops.js
@@ -0,0 +1,33 @@
+// for(i =0; i<30; i++){
+//     if(i===23){
+//         console.log("hello mr number 23")
+//     }
+//     console.log("hello" + i);
+// }
+
+//  we are toking about "FOR OF "=> loop 
+//  for of 
+//  ["" "" ""]
+//  [{}{}{}]
+
+//   const arr = [ 1,3,4,5,6]
+//    for(const num of arr){
+//     console.log(num);
+    
+//    }
+
+//     const  array = ["mohit ","kamal","ajay","aryan ","ram"]
+//     for(const name of array){
+//         console.log(`helloo  ${array}`);
+        
+//     }
+
+//      MAPS  IN JAVA SCRIPT
+ 
+ const  map = new Map()
+ map.set(`IND ` , "India")
+   
+  // console.log(map);
+       
+
+   
